df1f177f8d7ef2ce454d39c05d473007bf333511
PeterRobotica08 <114919881+PeterRobotica08@users.noreply.github.com>
2023-02-22 13:42:36 +0100

    Update Tarea musica "Quedate-Quevedo".py

--- Tarea musica "Quedate-Quevedo".py
+++ Tarea musica "Quedate-Quevedo".py
@@ -2,6 +2,6 @@
 from microbit import *
 import music
 
-notas = ['g5:8','f5:2','f5:4','f5:2','f5:2','g5:2','f5:2','e5:2','d5:2','f5:8']
+notas = ['g5:8','f5:2','f5:4','f5:2','f5:2','g5:2','f5:2','e5:2','d5:2','f5:8','e5:2','e5:3','d4:2','d4:2']
 
 music.play(notas)
